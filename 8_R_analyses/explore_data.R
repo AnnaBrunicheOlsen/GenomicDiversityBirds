5ec66e4d7784c597d6ff23bec300ff8eb9bcb83c
Ken Kellner <ken@kenkellner.com>
2019-05-01 13:57:59 -0400

    compare bird and mammal ROHs

--- 8_R_analyses/explore_data.R
+++ 8_R_analyses/explore_data.R
@@ -32,5 +32,57 @@ cov_data %>% select(-species) %>%
   cor(use='complete.obs') %>%
   round(.,3)
 
+# Compare roh stats between birds and mammals
+
+roh_mam <- read_excel('~/research/archive/runs_of_homozygosity/data/Cons_Gen_edits.xlsx')
 
 
+roh_mam2 <- read_csv('~/research/archive/runs_of_homozygosity/data/roh20X_full_02272018.csv')
+roh_mam2 <- rbind(roh_mam2,NA)
+
+roh_mam <- roh_mam[,c(1,5,6,10)]
+roh_mam <- cbind(roh_mam, roh_mam2$ROHnumber)
+names(roh_mam) <- c('species',
+                    'total_SNV','SNVS_in_scaffs_length_20snv',
+                    'scaffolds_min_20snv','rohNumber')
+
+
+roh_mam <- roh_mam %>%
+  mutate(taxa='mammal')
+
+roh_bird <- raw_genetics[,c(1,2,3,5,11)]
+names(roh_bird) <- c('species',
+                    'total_SNV','SNVS_in_scaffs_length_20snv',
+                    'scaffolds_min_20snv','rohNumber')
+roh_bird <- roh_bird %>%
+  mutate(taxa='bird')
+
+full <- rbind(roh_mam,roh_bird)
+
+total_SNV <- full %>% 
+  ggplot(aes(x=taxa, y=log(total_SNV), fill=taxa)) +
+  geom_violin(draw_quantiles=0.5) +
+  theme(legend.position='none')
+
+SNV_scaf_20 <- full %>%
+  ggplot(aes(x=taxa, y=log(SNVS_in_scaffs_length_20snv), fill=taxa)) +
+  geom_violin(draw_quantiles=0.5) +
+  theme(legend.position='none')
+
+scaf_min_20 <- full %>%
+  ggplot(aes(x=taxa, y=log(scaffolds_min_20snv), fill=taxa)) +
+  geom_violin(draw_quantiles=0.5) +
+  theme(legend.position='none')
+
+rohNum <- full %>%
+  ggplot(aes(x=taxa, y=log(rohNumber), fill=taxa)) +
+  geom_violin(draw_quantiles=0.5) +
+  theme(legend.position='none')
+
+library(gridExtra)
+
+pdf('bird_v_mam.pdf')
+
+grid.arrange(total_SNV,SNV_scaf_20, scaf_min_20, rohNum, ncol=2)
+
+dev.off()
